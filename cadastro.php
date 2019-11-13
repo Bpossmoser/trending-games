1d8d19632bb8b0375f09f0380918a62ac5528db2
victória <vicdudalopes15@gmail.com>
2019-11-13 08:42:42 -0400

    login

--- cadastro.php
+++ cadastro.php
@@ -8,8 +8,7 @@
         VALUES ('$nome', '$email', '$senha')";
     if(mysqli_query($conexao, $sql)){
         echo "<h1>Aluno cadastrado com sucesso</h1>";
-        echo "<a href='cadastro_aluno.html'>Cadastrar novo aluno?</a><br>";
-        echo "<a href='index.html'>Página inicial</a>";
+
     }
     else{
         echo "Error: ".$sql."<br>".mysqli_error($conexao);
